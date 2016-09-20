a31a9491b997a7405e467f5f9b3afc98565ff503
Profedelmodulo <profedelmodulo@gmail.com>
2016-09-20 16:50:00 +0100

    Ejemplos JS operaciones con numeros

--- example1.js
+++ example1.js
@@ -1,2 +1,13 @@
-console.log("Hola");
+var a = 3;
+var b = "asdf";
+var r1 = a * b;
+console.log(r1);
 
+var c;
+var r3 = a + c;
+console.log(r3);
+
+var d = "12";
+console.log(a * d);
+console.log(a + d);
+console.log(a + +d);
